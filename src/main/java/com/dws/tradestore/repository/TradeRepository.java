d134a8f199d2a285d1434500e55891adbf9abcf0
pankajmb4u <pankajmb4u@gmail.com>
2025-09-09 23:03:52 +0530

    Update TradeRepository.java

--- src/main/java/com/dws/tradestore/repository/TradeRepository.java
+++ src/main/java/com/dws/tradestore/repository/TradeRepository.java
@@ -2,8 +2,11 @@ package com.dws.tradestore.repository;
 
 import com.dws.tradestore.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface TradeRepository extends JpaRepository<Trade, Long> {
     Optional<Trade> findByTradeIdAndVersion(String tradeId, int version);
     Optional<Trade> findTopByTradeIdOrderByVersionDesc(String tradeId);
